5d41468ca61a90759fe87e3bd0763508a7b7f3f8
xiezhou07 <zhouxie811@gmail.com>
2024-11-17 12:26:35 +0800

    初始化

---
+++ src/main/java/com/dream/house/webauth_xielc/WebAuthXielcApplication.java
@@ -0,0 +1,13 @@
+package com.dream.house.webauth_xielc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAuthXielcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAuthXielcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dream/house/webauth_xielc/WebAuthXielcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dream.house.webauth_xielc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAuthXielcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
